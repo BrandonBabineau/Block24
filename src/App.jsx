8be7819a4a7e0dce4df6c2f5106dcb8bbb3f2333
Brandon Babineau <babineau1991@gmail.com>
2023-12-17 13:16:15 -0500

    final submission

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { puppyList } from './data.js';
 import { useState } from 'react';
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
